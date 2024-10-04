c3051e48fc3d2db7c48468c5078ceb9cb69542f8
purebver <wkddj1541@gmail.com>
2024-10-04 22:24:13 +0900

    Style: "필요없는 부분 제거"

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { createServer } from 'http';
 import initSocket from './init/socket.js';
 import { loadGameAssets } from './init/assets.js';
-import { createClient } from 'redis';
 
 const app = express();
 const server = createServer(app);
